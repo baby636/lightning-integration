9acb7214cf7b1ca93b909dccde33ecd21ebe73a8
Janus <ysangkok@gmail.com>
2018-12-07 20:16:03 +0100

    Use unlocker with LND

--- lnd.py
+++ lnd.py
@@ -19,8 +19,6 @@ os.environ["GRPC_SSL_CIPHER_SUITES"] = "ECDHE-ECDSA-AES256-GCM-SHA384"
 
 class LndD(TailableProc):
 
-    CONF_NAME = 'lnd.conf'
-
     def __init__(self, lightning_dir, bitcoind, port):
         super().__init__(lightning_dir, 'lnd({})'.format(port))
         self.lightning_dir = lightning_dir
@@ -31,40 +29,38 @@ class LndD(TailableProc):
         self.prefix = 'lnd'
 
         self.cmd_line = [
-            'bin/lnd',
+            '/home/janus/lightning-integration/bin/lnd',
             '--bitcoin.active',
             '--bitcoin.regtest',
-            '--datadir={}'.format(lightning_dir),
+            '--lnddir={}'.format(lightning_dir),
             '--debuglevel=trace',
             '--rpclisten=127.0.0.1:{}'.format(self.rpc_port),
             '--restlisten=127.0.0.1:{}'.format(self.rest_port),
             '--listen=127.0.0.1:{}'.format(self.port),
-            '--tlscertpath=tls.cert',
-            '--tlskeypath=tls.key',
             '--bitcoin.node=bitcoind',
             '--bitcoind.rpchost=127.0.0.1:{}'.format(BITCOIND_CONFIG.get('rpcport', 18332)),
             '--bitcoind.rpcuser=rpcuser',
             '--bitcoind.rpcpass=rpcpass',
             '--bitcoind.zmqpubrawblock=tcp://127.0.0.1:{}'.format(self.bitcoind.zmqpubrawblock_port),
             '--bitcoind.zmqpubrawtx=tcp://127.0.0.1:{}'.format(self.bitcoind.zmqpubrawtx_port),
-            '--configfile={}'.format(os.path.join(lightning_dir, self.CONF_NAME)),
             '--no-macaroons',
-            '--nobootstrap',
-            '--noseedbackup',
-            '--trickledelay=500'
         ]
 
-        if not os.path.exists(lightning_dir):
-            os.makedirs(lightning_dir)
-        with open(os.path.join(lightning_dir, self.CONF_NAME), "w") as f:
-            f.write("""[Application Options]\n""")
+    def make_channel(self):
+        with open(self.lightning_dir + '/tls.cert', 'rb') as f:
+            cred = grpc.ssl_channel_credentials(f.read())
+        return grpc.secure_channel('localhost:{}'.format(self.rpc_port), cred)
 
     def start(self):
         super().start()
         self.wait_for_log('RPC server listening on')
+        self.unlocker_stub = lnrpc_grpc.WalletUnlockerStub(self.make_channel())
+        seed = self.unlocker_stub.GenSeed(lnrpc.GenSeedRequest())
+        self.unlocker_stub.InitWallet(lnrpc.InitWalletRequest(wallet_password=b"password", recovery_window=0, cipher_seed_mnemonic=seed.cipher_seed_mnemonic))
         self.wait_for_log('Done catching up block hashes')
         time.sleep(5)
-
+        # need to remake the channel, otherwise the Lightning gRPC service might not be there yet
+        self.stub = lnrpc_grpc.LightningStub(self.make_channel())
         logging.info('LND started (pid: {})'.format(self.proc.pid))
 
     def stop(self):
@@ -84,7 +80,7 @@ class LndNode(object):
         self.bitcoin = bitcoind
         self.executor = executor
         self.daemon = LndD(lightning_dir, bitcoind, port=lightning_port)
-        self.rpc = LndRpc(self.daemon.rpc_port)
+        self.rpc = self.daemon
         self.logger = logging.getLogger('lnd-node({})'.format(lightning_port))
         self.myid = None
         self.node_id = node_id
@@ -100,14 +96,14 @@ class LndNode(object):
         Returns true if the node is reachable via RPC, false otherwise.
         """
         try:
-            self.rpc.stub.GetInfo(lnrpc.GetInfoRequest())
+            self.daemon.stub.GetInfo(lnrpc.GetInfoRequest())
             return True
         except Exception as e:
             print(e)
             return False
 
     def peers(self):
-        peers = self.rpc.stub.ListPeers(lnrpc.ListPeersRequest()).peers
+        peers = self.daemon.stub.ListPeers(lnrpc.ListPeersRequest()).peers
         return [p.pub_key for p in peers]
 
     def check_channel(self, remote):
@@ -115,7 +111,7 @@ class LndNode(object):
         """
         self_id = self.id()
         remote_id = remote.id()
-        channels = self.rpc.stub.ListChannels(lnrpc.ListChannelsRequest()).channels
+        channels = self.daemon.stub.ListChannels(lnrpc.ListChannelsRequest()).channels
         channel_by_remote = {c.remote_pubkey: c for c in channels}
         if remote_id not in channel_by_remote:
             self.logger.warning("Channel {} -> {} not found".format(self_id, remote_id))
@@ -127,7 +123,7 @@ class LndNode(object):
 
     def addfunds(self, bitcoind, satoshis):
         req = lnrpc.NewAddressRequest(type=1)
-        addr = self.rpc.stub.NewAddress(req).address
+        addr = self.daemon.stub.NewAddress(req).address
         bitcoind.rpc.sendtoaddress(addr, float(satoshis) / 10**8)
         self.daemon.wait_for_log("Inserting unconfirmed transaction")
         bitcoind.rpc.generate(1)
@@ -136,18 +132,18 @@ class LndNode(object):
         # The above still doesn't mean the wallet balance is updated,
         # so let it settle a bit
         i = 0
-        while self.rpc.stub.WalletBalance(lnrpc.WalletBalanceRequest()).total_balance == satoshis and i < 30:
+        while self.daemon.stub.WalletBalance(lnrpc.WalletBalanceRequest()).total_balance == satoshis and i < 30:
             time.sleep(1)
             i += 1
-        assert(self.rpc.stub.WalletBalance(lnrpc.WalletBalanceRequest()).total_balance == satoshis)
+        assert(self.daemon.stub.WalletBalance(lnrpc.WalletBalanceRequest()).total_balance == satoshis)
 
     def openchannel(self, node_id, host, port, satoshis):
-        peers = self.rpc.stub.ListPeers(lnrpc.ListPeersRequest()).peers
+        peers = self.daemon.stub.ListPeers(lnrpc.ListPeersRequest()).peers
         peers_by_pubkey = {p.pub_key: p for p in peers}
         if node_id not in peers_by_pubkey:
             raise ValueError("Could not find peer {} in peers {}".format(node_id, peers))
         peer = peers_by_pubkey[node_id]
-        self.rpc.stub.OpenChannel(lnrpc.OpenChannelRequest(
+        self.daemon.stub.OpenChannel(lnrpc.OpenChannelRequest(
             node_pubkey=codecs.decode(peer.pub_key, 'hex_codec'),
             local_funding_amount=satoshis,
             push_sat=0
@@ -158,7 +154,7 @@ class LndNode(object):
 
     def getchannels(self):
         req = lnrpc.ChannelGraphRequest()
-        rep = self.rpc.stub.DescribeGraph(req)
+        rep = self.daemon.stub.DescribeGraph(req)
         channels = []
 
         for e in rep.edges:
@@ -168,18 +164,18 @@ class LndNode(object):
 
     def getnodes(self):
         req = lnrpc.ChannelGraphRequest()
-        rep = self.rpc.stub.DescribeGraph(req)
+        rep = self.daemon.stub.DescribeGraph(req)
         nodes = set([n.pub_key for n in rep.nodes]) - set([self.id()])
         return nodes
 
     def invoice(self, amount):
         req = lnrpc.Invoice(value=int(amount/1000))
-        rep = self.rpc.stub.AddInvoice(req)
+        rep = self.daemon.stub.AddInvoice(req)
         return rep.payment_request
 
     def send(self, bolt11):
         req = lnrpc.SendRequest(payment_request=bolt11)
-        res = self.rpc.stub.SendPaymentSync(req)
+        res = self.daemon.stub.SendPaymentSync(req)
         if res.payment_error:
             raise ValueError(res.payment_error)
         return hexlify(res.payment_preimage)
@@ -187,10 +183,10 @@ class LndNode(object):
     def connect(self, host, port, node_id):
         addr = lnrpc.LightningAddress(pubkey=node_id, host="{}:{}".format(host, port))
         req = lnrpc.ConnectPeerRequest(addr=addr, perm=True)
-        logging.debug(self.rpc.stub.ConnectPeer(req))
+        logging.debug(self.daemon.stub.ConnectPeer(req))
 
     def info(self):
-        r = self.rpc.stub.GetInfo(lnrpc.GetInfoRequest())
+        r = self.daemon.stub.GetInfo(lnrpc.GetInfoRequest())
         return {
             'id': r.identity_pubkey,
             'blockheight': r.block_height,
@@ -215,11 +211,3 @@ class LndNode(object):
                 return False
             raise
         return True
-
-class LndRpc(object):
-
-    def __init__(self, rpc_port):
-        self.port = rpc_port
-        cred = grpc.ssl_channel_credentials(open('tls.cert', 'rb').read())
-        channel = grpc.secure_channel('localhost:{}'.format(rpc_port), cred)
-        self.stub = lnrpc_grpc.LightningStub(channel)
